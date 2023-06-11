0683aa53df0453b4519c1447971c8c36c989823e
fromiva <66008363+fromiva@users.noreply.github.com>
2023-06-11 21:10:09 +0300

    Initial commit

---
+++ src/test/java/com/github/fromiva/wsf/WebSharedFolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fromiva.wsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSharedFolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
